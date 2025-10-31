f60757d1e912286fb9698a8e805f1115e15cba30
Li Francucci <lisandro.francucci@gmail.com>
2025-10-31 16:24:07 -0300

    Change format of bookinstance's model due_back date with Luxon

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,3 +1,4 @@
+const { DateTime } = require('luxon')
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
@@ -21,5 +22,10 @@ BookInstanceSchema.virtual('url').get(function () {
     return `/catalog/bookinstance/${this._id}`
 })
 
+// Virtual for bookinstance's formatted Date
+BookInstanceSchema.virtual('due_back_formatted').get(function () {
+    return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED)
+})
+ 
 // Export the model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema)
