404d71d9da6153c213ecc61350fde77c7c589cef
Li Francucci <lisandro.francucci@gmail.com>
2025-10-31 11:28:52 -0300

    Add BookInstance routes

--- routes/catalog.js
+++ routes/catalog.js
@@ -3,10 +3,38 @@ const express = require('express')
 // Require controller modules
 const author_controller = require('../controllers/authorController')
 const book_controller = require('../controllers/bookController')
-const book_instance_controller = require('../controllers/bookinstanceController')
+const bookinstance_controller = require('../controllers/bookinstanceController')
 const genre_controller = require('../controllers/genreController')
 
 const router = express.Router()
+
+
+// AUTHOR ROUTES //
+
+// GET request for creating Author. NOTE This must come before routes that display author (uses id)
+router.get('/author/create', author_controller.author_create_get)
+
+// POST request for creating Author
+router.post('/author/create', author_controller.author_create_post)
+
+// GET request to delete Author
+router.get('/author/:id/delete', author_controller.author_delete_get)
+
+// POST request to delete Author
+router.post('/author/:id/delete', author_controller.author_delete_post)
+
+// GET request to update Author
+router.get('/author/:id/update', author_controller.author_update_get)
+
+// POST request to update Author
+router.post('/author/:id/update', author_controller.author_update_post)
+
+// GET request for one Author
+router.get('/author/:id', author_controller.author_detail)
+
+// GET request for list of all Authors
+router.get('/authors', author_controller.author_list)
+
 
 // BOOK ROUTES //
 
@@ -36,3 +64,30 @@ router.get('/book/:id', book_controller.book_detail)
 
 // GET request for list of all Book items
 router.get('/books', book_controller.book_list)
+
+
+// BOOKINSTANCE ROUTES //
+
+// GET request for creating a BookInstance. NOTE This must come before routes that display BookInstance (uses id)
+router.get('/bookinstance/create', bookinstance_controller.bookinstance_create_get)
+
+// POST request for creating BookInstance
+router.post('/bookinstance/create', bookinstance_controller.bookinstance_create_post)
+
+// GET request to delete BookInstance
+router.get('/bookinstance/:id/delete', bookinstance_controller.bookinstance_delete_get)
+
+// POST request to delete BookInstance
+router.post('/bookinstance/:id/delete', bookinstance_controller.bookinstance_delete_post)
+
+// GET request to update BookInstance
+router.get('/bookinstance/:id/update', bookinstance_controller.bookinstance_update_get)
+
+// POST request to update BookInstance
+router.post('/bookinstance/:id/update', bookinstance_controller.bookinstance_update_post)
+
+// GET request for one BookInstance
+router.get('/bookinstance/:id', bookinstance_controller.bookinstance_detail)
+
+// GET request for list of all BookInstance items
+router.get('/bookinstances', bookinstance_controller.bookinstance_list)
