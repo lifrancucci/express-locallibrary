6a6cf9601e7934bb611cf1c17761ef3aeb18341e
Li Francucci <lisandro.francucci@gmail.com>
2025-10-31 11:31:02 -0300

    Add Genre routes and export

--- routes/catalog.js
+++ routes/catalog.js
@@ -91,3 +91,32 @@ router.get('/bookinstance/:id', bookinstance_controller.bookinstance_detail)
 
 // GET request for list of all BookInstance items
 router.get('/bookinstances', bookinstance_controller.bookinstance_list)
+
+
+// GENRE ROUTES //
+
+// GET request for creating a Genre. NOTE This must come before routes that display Genre (uses id)
+router.get('/genre/create', genre_controller.genre_create_get)
+
+// POST request for creating Genre
+router.post('/genre/create', genre_controller.genre_create_post)
+
+// GET request to delete Genre
+router.get('/genre/:id/delete', genre_controller.genre_delete_get)
+
+// POST request to delete Genre
+router.post('/genre/:id/delete', genre_controller.genre_delete_post)
+
+// GET request to update Genre
+router.get('/genre/:id/update', genre_controller.genre_update_get)
+
+// POST request to update Genre
+router.post('/genre/:id/update', genre_controller.genre_update_post)
+
+// GET request for one Genre
+router.get('/genre/:id', genre_controller.genre_detail)
+
+// GET request for list of all Genre items
+router.get('/genres', genre_controller.genre_list)
+
+module.exports = router
