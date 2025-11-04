686ff38523f25836c02cb9c365f45619d5703ef4
Li Francucci <lisandro.francucci@gmail.com>
2025-11-03 21:06:27 -0300

    Create functions for displaying and creating Book Instance in bookinstanceController, and corresponding bookinstance_form view

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,6 @@
-const BookInstance = require("../models/bookinstance");
+const BookInstance = require('../models/bookinstance')
+const Book = require('../models/book')
+const { body, validationResult } = require('express-validator')
 
 // Display list of all BookInstances.
 exports.bookinstance_list = async (req, res, next) => {
@@ -12,7 +14,7 @@ exports.bookinstance_list = async (req, res, next) => {
     title: 'Book Instance List',
     bookinstance_list: allBookInstances,
   })
-};
+}
 
 // Display detail page for a specific BookInstance.
 exports.bookinstance_detail = async (req, res, next) => {
@@ -31,34 +33,82 @@ exports.bookinstance_detail = async (req, res, next) => {
     title: 'Book',
     bookinstance: bookInstance,
   })
-};
+}
 
 // Display BookInstance create form on GET.
 exports.bookinstance_create_get = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance create GET");
-};
+  // Get all books
+  const allBooks = await Book.find({}, 'title').sort({ title: 1 }).exec()
+
+  res.render('bookinstance_form', {
+    title: 'Create Book Instance',
+    book_list: allBooks,
+  })
+}
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance create POST");
-};
+exports.bookinstance_create_post = [
+  // Validate and sanitize fields
+  body('book', 'Book must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('status')
+    .escape(),
+  body('due_back', 'Invalid date')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization
+  async (req, res, next) => {
+    // Extract the validation errors from a request
+    const errors = validationResult(req) 
+
+    // Create a BookInstance object with escaped and trimmed data 
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    })
+
+    if(!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages
+      const allBooks = await Book.find({}, 'title').sort({ title: 1 }).exec()
+
+      res.render('bookinstance_form', {
+        title: 'Create Book Instance',
+        book_list: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookInstance,
+      })
+      return
+    }
+
+    // Data from form is valid
+    await bookInstance.save()
+    res.redirect(bookInstance.url)
+  }
+]
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = async (req, res, next) => {
   res.send("NOT IMPLEMENTED: BookInstance delete GET");
-};
+}
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = async (req, res, next) => {
   res.send("NOT IMPLEMENTED: BookInstance delete POST");
-};
+}
 
 // Display BookInstance update form on GET.
 exports.bookinstance_update_get = async (req, res, next) => {
   res.send("NOT IMPLEMENTED: BookInstance update GET");
-};
+}
 
 // Handle bookinstance update on POST.
 exports.bookinstance_update_post = async (req, res, next) => {
   res.send("NOT IMPLEMENTED: BookInstance update POST");
-};
+}
