617433025a0cc4082b5b1d073b51692cefdb86ff
Li Francucci <lisandro.francucci@gmail.com>
2025-11-06 20:24:06 -0300

    Create function to delete Genre in genreController, and corresponding genre_delete view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -86,12 +86,46 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+  // Get details genre and all books of the genre (in parallel)
+  const [ genre, allBooksInGenre ] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, 'title summary').exec(),
+  ])
+
+  if (genre === null) {
+    // No results 
+    res.redirect('/catalog/genres')
+    return
+  }
+
+  res.render('genre_delete', {
+    title: 'Delete Genre',
+    genre,
+    books_in_genre: allBooksInGenre,
+  })
 }
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
+  // Get details genre and all books of the genre (in parallel)
+  const [ genre, allBooksInGenre ] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, 'title summary').exec(),
+  ])
+
+  if (allBooksInGenre.length > 0) {
+    // There are books in this genre. Render in same way as for GET route
+    res.render('genre_delete', {
+      title: 'Delete Genre',
+      genre,
+      books_in_genre: allBooksInGenre,
+    })
+    return
+  }
+
+  // There are no books in this genre. Delete object and redirect to the list of genres
+  await Genre.findByIdAndDelete(req.body.genreid)
+  res.redirect('/catalog/genres')
 }
 
 // Display Genre update form on GET.
